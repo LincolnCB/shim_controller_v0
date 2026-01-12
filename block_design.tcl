7f4241d63055aee2f794218c86accf4c439493eb
LincolnCB-virt <lcravenbrightman@gmail.com>
2026-01-12 13:22:06 -0500

    Updated for zynq_toolbox core refactoring

--- block_design.tcl
+++ block_design.tcl
@@ -168,7 +168,7 @@ wire shim_dac_0/spi_sequencer_0/spi_ref_clk ps_0/FCLK_CLK0
 ## Connect output buffers
 
 # Differential output buffer for CS
-cell lcb:user:diff_out_buf:1.0 cs_o_buf {
+cell base:user:diff_out_buf:1.0 cs_o_buf {
   DIFF_BUF_WIDTH 1
 } {
   d_in /shim_dac_0/spi_sequencer_0/spi_cs
@@ -176,7 +176,7 @@ cell lcb:user:diff_out_buf:1.0 cs_o_buf {
   diff_out_n cs_o_n
 }
 # Differential output buffer for SPI clock
-cell lcb:user:diff_out_buf:1.0 spi_clk_o_buf {
+cell base:user:diff_out_buf:1.0 spi_clk_o_buf {
   DIFF_BUF_WIDTH 1
 } {
   d_in /shim_dac_0/spi_sequencer_0/spi_clk
@@ -191,7 +191,7 @@ cell xilinx.com:ip:util_vector_logic ldac_inv {
 } {
   Op1 /shim_dac_0/spi_sequencer_0/spi_ldacn
 }
-cell lcb:user:diff_out_buf:1.0 ldac_o_buf {
+cell base:user:diff_out_buf:1.0 ldac_o_buf {
   DIFF_BUF_WIDTH 1
 } {
   d_in ldac_inv/Res
@@ -199,7 +199,7 @@ cell lcb:user:diff_out_buf:1.0 ldac_o_buf {
   diff_out_n ldac_o_n
 }
 # Differential output buffer for DAC MOSI
-cell lcb:user:diff_out_buf:1.0 dac_mosi_o_buf {
+cell base:user:diff_out_buf:1.0 dac_mosi_o_buf {
   DIFF_BUF_WIDTH 4
 } {
   d_in /shim_dac_0/spiconcat_0/Dout
